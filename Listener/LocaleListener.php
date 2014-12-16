0bf1a9b798853b76abd30f649983e581b21e91af
Vincent Chalamon <vincentchalamon@gmail.com>
2014-12-16 09:23:41 +0100

    add locale listener

---
+++ Listener/LocaleListener.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the VinceCms bundle.
+ *
+ * (c) Vincent Chalamon <http://www.vincent-chalamon.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Vince\Bundle\CmsBundle\Listener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+/**
+ * Listen to kernel.request event to save locale
+ *
+ * @author Vincent Chalamon <vincentchalamon@gmail.com>
+ */
+class LocaleListener
+{
+
+    /**
+     * Locale
+     *
+     * @var string
+     */
+    protected $locale;
+
+    /**
+     * Log kernel.request event
+     *
+     * @author Vincent Chalamon <vincentchalamon@gmail.com>
+     *
+     * @param GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $this->locale = $event->getRequest()->getLocale();
+    }
+
+    /**
+     * Get locale
+     *
+     * @return string
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+}
